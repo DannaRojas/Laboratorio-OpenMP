7bc90d127a0e8f4f151a666bc83179622dcb6a51
Danna Gabriela Rojas Bernal <65837321+DannaRojas@users.noreply.github.com>
2025-11-07 16:15:15 -0500

    Lab05Rojas.c

--- Lab05Rojas.c
+++ Lab05Rojas.c
@@ -1,23 +1,23 @@
-/**************************************************
-*       Pontificia Universidad Javeriana          *
-*               ----------=----------             *
-*                                                 *
-* Autor: Danna Gabriela Rojas Bernal              *
+/****************************************************
+*       Pontificia Universidad Javeriana            *
+*               ----------=----------               *
+*                                                   *
+* Autor: Danna Gabriela Rojas Bernal                *
 * Fecha: 6 noviembre de 2025                        *
-* Docente: J. Corredor
-* Materia: Sistemas Operativos                             *
-* Tema :Introducción a OpenMP
-*Descripcion: Uso de la directiva y funcion 
-               para hacer uso de OpenMP 
-	     -Paralelismo para el calculo de la 
-	     funcion seno              *
-***************************************************/
+* Docente: J. Corredor								*
+* Materia: Sistemas Operativos                      *
+* Tema :Introducción a OpenMP						*
+* Descripcion: Uso de la directiva y funcion 		*
+* para hacer uso de OpenMP 							*
+*    -Paralelismo para el calculo de la funcion seno*              
+*****************************************************/
 
-
+//librerias
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
 #include <math.h>
+//define funcion para obtener el minimo 
 #define MIN(x,y) (((x) < (y)) ? (x):(y))
 
 //se implementa la funcion sumatoria
@@ -30,18 +30,18 @@ double fsum(int i){
 	}
 	return valorSum;
 }
-
+//programa principal
 int main(int argc, char *argv[]){
 	
-double resSumatoria=0;
+	double resSumatoria=0;
 	int rep = 30000;
 	if (argc!=2){
 	printf("Error: \n\t$. /ejecutable numHilos\n");
 	exit(0);
 	}
-	int numHilos= (int) atoi(argv[1]);
+	int numHilos= (int) atoi(argv[1]);//conversion del argumento recibido
 	
-	int maxCores= omp_get_max_threads();
+	int maxCores= omp_get_max_threads();// Se obtiene el número máximo de hilos soportados por el sistema
 	printf("Número maximo de Cored del SO: %d \n", maxCores);
 	
 	// Se fijan el número de hilos deseado: numHilos
@@ -52,6 +52,8 @@ double resSumatoria=0;
 	//región paralela OMP
 	#pragma omp parallel
 {
+	//segura que cada hilo tenga una copia privada de 'resSumatoria' y las combine al final
+
 	#pragma omp parallel for reduction (+: resSumatoria)
 	for(int i=0; i<rep; i++)
 	resSumatoria +=fsum(i);
