ffb5d5bf850f224039c6d0984ffcf61091cc857e
Danna Gabriela Rojas Bernal <65837321+DannaRojas@users.noreply.github.com>
2025-11-07 16:00:19 -0500

    Lab02Rojas.c

--- Lab02Rojas.c
+++ Lab02Rojas.c
@@ -3,30 +3,32 @@
 *               ----------=----------             *
 *                                                 *
 * Autor: Danna Gabriela Rojas Bernal              *
-* Fecha: 6 noviembre de 2025                        *
-* Docente: J. Corredor
-* Materia: Sistemas Operativos                             *
-* Tema :Introducción a OpenMP
-*Descripcion: Uso de la directiva y funcion 
-               para hacer uso de OpenMP              *
+* Fecha: 6 noviembre de 2025                      *
+* Docente: J. Corredor							  *
+* Materia: Sistemas Operativos                    *
+* Tema :Introducción a OpenMP					  *
+* Descripcion: Uso de la directiva y funcion 	  *
+* para hacer uso de OpenMP                        *
 ***************************************************/
 
-
+//Libreria 
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
 
+//programa principal 
 int main(int argc, char *argv[]){
-if (argc!=2){
+if (argc!=2){//verificacion que lleguen los argumentos 
 	printf("Error: \n\t$. /ejecutable numHilos\n");
 	exit(0);
 }
 	int numHilos= (int) atoi(argv[1]);
 
+	// Se obtiene el número máximo de hilos (threads) que el sistema tiene disponibles por defecto
 	int maxCores= omp_get_max_threads();
 	printf("Número maximo de Cored del SO: %d \n", maxCores);
-	// Se fijan el número de hilos deseado: numHilos
-	omp_set_num_threads(numHilos);
+	// Se fijan el número de hilos deseado
+	omp_set_num_threads(numHilos);//
 	printf("Número de hilos fijados: %d \n", numHilos);
 
 //región paralela OMP
@@ -36,5 +38,6 @@ if (argc!=2){
 }
 return 0;
 }
-
+//Analisis: puede colocar más hilos de los que tiene el procesor ya que el OpenMP crea hilos logicos
+// por lo que el SO que varios hilos compartan los mismos núcleos
 
