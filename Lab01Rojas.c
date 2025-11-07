f1d2270827e95985266f9a97a7cdcb8c5e036165
Danna Gabriela Rojas Bernal <65837321+DannaRojas@users.noreply.github.com>
2025-11-07 15:51:28 -0500

    Lab01Rojas.c

--- Lab01Rojas.c
+++ Lab01Rojas.c
@@ -3,25 +3,26 @@
 *               ----------=----------             *
 *                                                 *
 * Autor: Danna Gabriela Rojas Bernal              *
-* Fecha: 6 noviembre de 2025                        *
-* Docente: J. Corredor
-* Materia: Sistemas Operativos                             *
-* Tema :Introducción a OpenMP
-*Descripcion: Uso de la directiva y funcion 
-               para hacer uso de OpenMP              *
+* Fecha: 6 noviembre de 2025                      *
+* Docente: J. Corredor							  *
+* Materia: Sistemas Operativos                    *
+* Tema :Introducción a OpenMP					  *
+* Descripcion: Uso de la directiva y funcion 	  *
+* para hacer uso de OpenMP                        *
 ***************************************************/
 
-
+//Libreria
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
 
+//Programa principal
 int main(int argc, char *argv[]){
-	int maxCores= omp_get_max_threads();
+	
+int maxCores= omp_get_max_threads(); // para saber cuántos hilos puede usar
 printf("Número maximo de Cored del SO: %d \n", maxCores);
 
 //región paralela OMP
-
 #pragma omp parallel
 {
 	printf("Hilo ID en Región paralela: %d \n", omp_get_thread_num());
